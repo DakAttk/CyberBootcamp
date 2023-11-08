9ba5ac4976593f461e4043c8f0883512b02c1ece
DakAttk <dakotah.m.carroll@gmail.com>
2023-11-08 13:00:59 -0700

    update

---
+++ 201ops3.sh
@@ -0,0 +1,14 @@
+#echo Choose your number
+
+
+number=$1
+
+if [ $number -eq 5 ]; then
+	echo "The number is equal to 5"
+fi
+if [ $number -lt 5 ]; then
+	echo "The number is less than 5"
+fi
+if [ $number -gt 5 ]; then
+	echo "The number is greater than 5"
+fi
