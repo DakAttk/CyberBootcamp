17f4efd5054b8446f72cb5876468ff41f0b37ae9
DakAttk <dakotah.m.carroll@gmail.com>
2023-11-08 16:00:59 -0700

    update

--- Unit 103/201ops3.sh
+++ Unit 103/201ops3.sh
@@ -3,9 +3,9 @@ number=$1
 if [ $number -eq 5 ]; then
         echo "The number is equal to 5"
 fi
-if [ $number -lt 5 ]; then
+elif [ $number -lt 5 ]; then
         echo "The number is less than 5"
 fi
-if [ $number -gt 5 ]; then
+elif [ $number -gt 5 ]; then
         echo "The number is greater than 5"
 fi
