85737fae08428e63998f1056efb8610f23321bbe
Dakotah Carroll <147547226+DakAttk@users.noreply.github.com>
2024-01-03 18:02:03 -0700

    Create ops4.py

---
+++ Unit 104/ops4.py
@@ -0,0 +1,22 @@
+# whoami
+# ip a
+# lshw -short
+# Stretch Goals (Optional Objectives)
+# Pursue stretch goals if you are a more advanced user or have remaining lab time.
+# Instead of os.system() function, utilize the subprocess module instead. Refer to python.org for how this can be achieved.
+
+
+
+# This will not run on windows needs to be mac or linux due to os being bash
+
+import subprocess
+import os
+n="ls"
+a="whoami"
+b="ifconfig"
+os.system
+subprocess.run(n)
+print ()
+subprocess.run(a)
+print()
+subprocess.run(b)
