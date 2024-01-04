afc5a2c3ad7a83c82d078aedfc289dcc7ad9ab9f
Dakotah Carroll <147547226+DakAttk@users.noreply.github.com>
2024-01-03 18:01:13 -0700

    Create ops3.py

---
+++ Unit 104/ops3.py
@@ -0,0 +1,8 @@
+# Using one of the comparison operators in Python, write a simple program that takes the parameter n as input,
+# which is an integer, and prints False if n is less than 100, and True if n is greater than or equal to 100.
+
+x = 500
+if (x >= 300):
+    print (True)
+if (x <= 200):
+    print (False)
